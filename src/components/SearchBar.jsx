845828b649c6e91f51cacc94537c25d2323e2c65
veekay17 <vijay.vuex@gmail.com>
2023-07-04 23:11:14 +0530

    focus input automatically for searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,23 @@
+import { useEffect, useRef, useState } from "react";
 
 function SearchBar() {
+  const [inputValue, setInputValue] = useState("");
+  const inputRef = useRef();
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+  // console.log(inputValue)
   return (
-    <input type="text" placeholder="search for an item" className="p-3 w-20 md:w-full md:mx-3 border-2 border-secondary-300 rounded-lg  "></input>
-  )
+    <input
+      ref={inputRef}
+      type="text"
+      value={inputValue}
+      onChange={(e) => setInputValue(e.target.value)}
+      placeholder="search for an item"
+      className="p-3 w-20 md:w-full md:mx-3 border-2 border-secondary-300 rounded-lg  "
+    ></input>
+  );
 }
 
-export default SearchBar
+export default SearchBar;
