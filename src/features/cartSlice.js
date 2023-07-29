1f84d07ab4c731b1a3e536247ebc5bd5a585746e
veekay17 <vijay.vuex@gmail.com>
2023-07-29 15:57:56 +0530

    created store and cartSlice using redux toolkit to replace context

---
+++ src/features/cartSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    cart: []
+}
+
+const cartSlice = createSlice({
+    name: "cart",
+    initialState,
+    reducers:{
+        addProduct: (state,action) => {
+            const existingProduct = state.cart.find((product) => product.id === action.payload.id);
+            if(existingProduct){
+                existingProduct.quantity++
+            }else{
+              state.cart.push({...action.payload, quantity: 1 })  
+            }
+        }
+    }
+})
+
+const cartReducer = cartSlice.reducer
+const addProduct = cartSlice.actions
+export {addProduct,cartReducer}
